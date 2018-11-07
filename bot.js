258c5b823bdedc3ccd59667db69a061b005203a3
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-11-07 20:53:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,11 +7,11 @@ client.on('ready', () => {
 
 
 client.on('ready', async() => {
-var server = "508255957629796372"; // ايدي السررفر
-var channel = "508255957629796374";//ايدي الروم
+var server = "509816177497866271"; // ايدي السررفر
+var channel = "509816177497866273";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , ')
     },305);
 })
 
-client.login('NTA3OTE2MjIyOTAyMzA0Nzc4.Dr8mwA.GLjv7n29VHP-vzYEnqKE-EgOK2Y');
+client.login('NTA5ODE2NDM2Mzk5NTM4MTc2.DsTUKg.FS7AXJo-83JY14wkU4BOEhPwXHA');
